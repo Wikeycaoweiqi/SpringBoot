75d8c7015e64fcd86704c69b31c5c326cdffd288
Wikey Cao <wikeycaoweiqi@163.com>
2024-07-09 23:46:14 +0800

    quick start spring boot

---
+++ springboot03_demo01/src/main/java/com/wikeyspringboot/springboot03_demo01/Springboot03Demo01Application.java
@@ -0,0 +1,13 @@
+package com.wikeyspringboot.springboot03_demo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot03Demo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot03Demo01Application.class, args);
+	}
+
+}
---
+++ springboot03_demo01/src/test/java/com/wikeyspringboot/springboot03_demo01/Springboot03Demo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wikeyspringboot.springboot03_demo01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot03Demo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

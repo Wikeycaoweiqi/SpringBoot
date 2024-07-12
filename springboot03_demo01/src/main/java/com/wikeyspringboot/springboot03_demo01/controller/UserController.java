1b585a26448ebec024a7052f038541f7400352e8
Wikey Cao <caoweiqi@haiyisoft.com>
2024-07-12 17:30:20 +0800

    springboot demo01 study

--- springboot03_demo01/src/main/java/com/wikeyspringboot/springboot03_demo01/controller/UserController.java
+++ springboot03_demo01/src/main/java/com/wikeyspringboot/springboot03_demo01/controller/UserController.java
@@ -3,7 +3,9 @@ package com.wikeyspringboot.springboot03_demo01.controller;
 import com.wikeyspringboot.springboot03_demo01.pojo.Result;
 import com.wikeyspringboot.springboot03_demo01.pojo.User;
 import com.wikeyspringboot.springboot03_demo01.service.UserService;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2024-07-10 23:00
  * @Description:
  */
+@Validated
 @RestController
 @RequestMapping(value = "/user")
 public class UserController {
@@ -22,7 +25,7 @@ public class UserController {
     UserService userService;
 
     @PostMapping(value = "register")
-    public Result register(@RequestParam("username") String username, @RequestParam("password") String password) {
+    public Result register(@RequestParam("username") @Pattern(regexp = "^\\S{5,16}$") String username, @RequestParam("password") @Pattern(regexp = "^\\S{5,16}$") String password) {
         // 查询用户
         User user = userService.findUserByUsername(username);
         if (user == null) {
